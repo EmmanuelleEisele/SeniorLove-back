4782f11e878ffe65ee846e52b8fb360f9d9ce20a
EmmanuelleEisele <manuecam2b@hotmail.fr>
2025-08-25 14:34:01 +0200

    Delete senior-love-back-dev/senior-love-back-dev directory
    Delete dev

--- senior-love-back-dev/senior-love-back-dev/README.md
+++
@@ -1,4 +0,0 @@
-# Projet SeniorLove (site de rencontre pour +60):
-
-## Sprint 0:
-Les éléments se trouvent dans [docs](docs)
--- senior-love-back-dev/senior-love-back-dev/src/models/sequelize.js
+++
@@ -1,7 +0,0 @@
-import { Sequelize } from "sequelize";
-import 'dotenv/config';
-
-
-export const sequelize = new Sequelize(process.env.PG_URL, {
-  dialect: "postgres"
-});
